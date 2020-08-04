c0874ac895138a07e448ae9dcf3b40c47bea0add
Jose Angel <jas@deb>
2020-08-04 01:08:57 -0400

    Practical use of widget Scaffold and some properties

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Center(
-      child: Text('Well, this is the beginning!'),
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[200],
+      appBar: AppBar(
+        title: Text('I AM RICH'),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRvg5nq4WcLZ_8adB09W_YQj8ICb60WY-KCRA&usqp=CAU'),
+        ),
+      ),
     ),
   ));
 }
