7ea56496352b882c8ffc71e94da86ac43042108f
Jose Angel <jas@deb>
2020-08-04 12:43:06 -0400

    Use of the assetImage

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,10 @@ void main() {
         backgroundColor: Colors.blueGrey[900],
       ),
       body: Center(
-        child: Image(
-          image: NetworkImage(
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRvg5nq4WcLZ_8adB09W_YQj8ICb60WY-KCRA&usqp=CAU'),
-        ),
-      ),
+          child: Image(image: AssetImage('images/diamond.png')
+              /* NetworkImage(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRvg5nq4WcLZ_8adB09W_YQj8ICb60WY-KCRA&usqp=CAU'),*/
+              )),
     ),
   ));
 }
